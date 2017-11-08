f3501c37717cbd0cdf1fb70dae81a157cee9f03f
Trevor Langston <trevor@Trevors-MacBook-Pro.local>
2017-11-08 15:20:47 -0800

    Add some argument tests

--- clone.sh
+++ clone.sh
@@ -1,5 +1,17 @@
+if [ -z "$1" ]
+  then
+    echo "No git url supplied"
+    exit 1
+fi
+
 domain=$(echo "$1" | grep -oe '@.*:' | tr -d @:)
 user=$(echo "$1" | grep -oe ':.*/' | tr -d /:)
+if [ -z "$domain" ] || [ -z "$user" ]
+  then
+    echo "The url must use the git protocol"
+    exit 1
+fi
+
 mkdir -p  ~/src/"$domain"/"$user"
 cd ~/src/"$domain"/"$user"
 git clone $1
